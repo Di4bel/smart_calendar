f32518c59184d810fef04aa4da394556f5856daa
Diabel <diabel@diabel.net>
2023-08-13 00:05:21 +0200

    import of the connect.php and an example with timestamps

--- Database/Database.php
+++ Database/Database.php
@@ -1,12 +1,58 @@
 <?php
 namespace smartCalendar\Database;
-
-class Database extends \mysqli
+require_once "connect.php";
+class Database
 {
     private $database;
-    public function __construct($hostname = "localhost", $username = "al",$password = "al",$database = "smart_calendar")
+
+    public function __construct()
     {
-        $this->database = parent::__construct($hostname, $username, $password, $database);
+        global $hostname;
+        global $username;
+        global $password;
+        global $database;
+        $this->database = new \mysqli($hostname,$username,$password,$database);
+    }
+
+    public function createNewUser($firstname, $lastname, $email, $password, $bio){
+        if (!is_string($firstname) && !is_string($lastname) && !is_string($email) && !is_string($bio)){
+            return "Input not a string";
+        }
+        $sanitized_firstname = htmlspecialchars($firstname);
+        $sanitized_lastname = htmlspecialchars($lastname);
+        $sanitized_email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        $sanitized_password = password_hash(htmlspecialchars($password),PASSWORD_ARGON2ID);
+        $sanitized_bio = htmlspecialchars($bio);
+
+
+        try {
+            $this->database->query("Insert into users (firstname, lastname, email, password, bio) value ( '$sanitized_firstname', '$sanitized_lastname', '$sanitized_email', '$sanitized_password', '$sanitized_bio')");
+            echo "User added!";
+
+        }catch (\Exception $e){
+            echo $e->getMessage();
+        }
+    }
+    public function confirmUser($email, $password)
+    {
+        $sanitizeEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
+        $sanitized_password = htmlspecialchars($password);
+        try {
+            $query = $this->database->query("Select password from users where email = '$sanitizeEmail'");
+            $databaseResponse = $query->fetch_assoc();
+            $databasePassword = $databaseResponse["password"];
+
+        } catch (\Exception $e) {
+            echo $sanitizeEmail . " not in the database!";
+            return;
+        }
+
+        if (password_verify($sanitized_password, $databasePassword)) {
+            $result = "richtig";
+        } else {
+            $result = "falsch";
+        }
+        echo $result;
     }
 
 
@@ -14,7 +60,7 @@ class Database extends \mysqli
 date_default_timezone_set('Europe/Berlin');
 $timestamp = mktime(18,0,0,8,12,2023);
 $timestamp2 = mktime(19,0,0,8,12,2023);
-// date("H") -> für 24h
+// date("H") makes the 18 hours to 18:00 and date("h") -> to 6:00 because of "p.m."
 $query = "Insert into events (name, info, timeBegin, timeEnd) value ('Programmieren','Projekt weiter programmieren','".date("Y-m-d H:i:s", $timestamp ). "','".date("Y-m-d H:i:sa", $timestamp2 )."')";
+
 $test = new Database();
-$test->query($query);
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # smart_calendar
 a personal calendar-site
+
+---
+
+start with opening the "iniDatabase.php"-File in the browser and create with it a "connect.php"-File with the database-Data.
